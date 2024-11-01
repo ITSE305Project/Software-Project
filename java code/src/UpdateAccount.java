f6b32d33966d0486ab652e4068baa88b2d12b795
FatimaMatrook <202006773@stu.uob.edu.bh>
2024-11-02 00:21:08 +0300

    Update

--- java code/src/UpdateAccount.java
+++ java code/src/UpdateAccount.java
@@ -7,78 +7,42 @@ class updateAccount {
     private int password; // Password
     private String email; // Email
     private int pNumber; // Phone Number
-    private String location;
+    private String location; // location
 
     // Constructor to initialize a Customer object
-    public Customer(String firstName, String lastName, int password, 
+    public updateAccount(String firstName, String lastName, int password, 
                     String email, int pNumber, String location) {
-        this.fName = firstName;
-        this.lName = lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.password = password;
         this.email = email;
         this.pNumber = pNumber;
-        this.location = location;
-    }
+        this.location = location;}
+
 
     // Getters for customer details
-    public String getFName() {
-        return firstName;
-    }
-
-    public String getLName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-    public int getPNumber() {
-        return pNumber;
-    }
-
-    public String getLocation() {
-        return location;
-    }
+    public String getFName() {return firstName;}
+    public String getLName() {return lastName;}
+    public String getEmail() {return email;}
+    public int getPassword() {return password;}
+    public int getPNumber() {return pNumber;}
+    public String getLocation() {return location;}
 
     // Setters for customer details
-    public void setFName(String fName) {
-        this.fName = firstName;
-    }
+    public void setFName(String firstName) {this.firstName = firstName;}
+    public void setLName(String lastName) {this.lastName = lastName;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPassword(int password) {this.password = password;}
+    public void setPNumber(int pNumber) {this.pNumber = pNumber;}
+    public void setLocation(String location) { this.location = location;}
 
-    public void setLName(String lName) {
-        this.lName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setPNumber(String pNumber) {
-        this.pNumber = pNumber;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-}
-
+                    }
 // Business Layer: Class to handle account management
 class AccountManager {
     private Customer customer;
 
     // Constructor to initialize the AccountManager with a Customer
-    public AccountManager(Customer customer) {
-        this.customer = customer;
-    }
+    public AccountManager(Customer customer) {this.customer = customer;}
 
     // presentation layer: update account details
     public void updateAccount() {
@@ -102,7 +66,7 @@ class AccountManager {
             customer.setEmail(email);
 
         System.out.print("Enter new phone number: ");
-        String pNumber = kbd.nextLine();
+        int pNumber = kbd.nextLine();
         if (!pNumber.isEmpty())
             customer.setPNumber(pNumber);
 
@@ -111,6 +75,6 @@ class AccountManager {
         if (!location.isEmpty())
             customer.setLocation(location);
 
-        System.out.println("Account updated successfully!");
+        System.out.println("Account updated successfully!");}
     }
 }
