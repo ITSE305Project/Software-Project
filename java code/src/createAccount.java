2e8a13299e864e34022f61311a7025d3e28c02fa
Ahmed Alwadyani <20197363@stu.uob.edu.bh>
2024-11-01 18:27:42 +0300

    add java code for the project

---
+++ java code/src/createAccount.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+// this class is to represnt customer with his details
+class Customer {
+    private String fName; // First Name
+    private String lName; // Last Name
+    private String username;
+    private String password;
+    private String email;
+    private String pNumber; // Phone Number
+    private String location;   
+
+        // Constructor to initialize a new Customer object
+        public Customer(String fName, String lName, String username, String password, 
+        String email, String pNumber, String location){
+            this.fName = fName;
+            this.lName = lName;
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.pNumber = pNumber;
+            this.location = location;
+        }
+
+                // this method is to print customer information in readable format
+                public String toString(){
+                    return "Customer{" +
+                                "First Name='" + fName + '\'' +
+                                ",Last Name='" + lName + '\'' +
+                                ",username='" + username + '\'' +
+                                ",email='" + email + '\'' +
+                                ",phone Number='" + pNumber + '\'' +
+                                ",location='" + location + '\'' +
+                                '}';
+                }
+}
+
+// this class to hunde account creation process
+class createAccount{
+    
+    // this method to get the input informations from the user
+    public static Customer createNewAccount(){
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String fName = kbd.nextLine();
+        System.out.println("Enter your last name:  ");
+        String lName = kbd.nextLine();
+        System.out.println("Enter your username: ");
+        String username = kbd.nextLine();
+        System.out.println("Enter your password: ");
+        String password = kbd.nextLine();
+        System.out.println("Enter your email: ");
+        String email = kbd.nextLine();
+        System.out.println("Enter your phone number: ");
+        String pNumber = kbd.nextLine();
+        System.out.println("Enter your location: ");
+        String location = kbd.nextLine();
+        
+        // this to create and return new customer object with the informations that user input it
+        return new Customer(fName, lName, username, password, email, pNumber, location);
+    }
+}
+
+
---
+++ java code/src/testCreateAccount.java
@@ -0,0 +1,12 @@
+public class testCreateAccount {
+    
+        public static void main(String[] args) {
+
+            // to call the method and store it in newCustomer
+            Customer newCustomer =createAccount.createNewAccount();
+
+            // display the customer details
+            System.out.println(newCustomer);
+        }
+    
+}
